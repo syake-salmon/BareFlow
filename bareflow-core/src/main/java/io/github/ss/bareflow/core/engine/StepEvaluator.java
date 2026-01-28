7309087c4cfd295ef8dc5d0ec812cb1e4c989398
ASAMI Keisuke <syakeapps@icloud.com>
2026-01-28 19:48:44 +0900

    Impl StepEvaluator

--- bareflow-core/src/main/java/io/github/ss/bareflow/core/engine/StepEvaluator.java
+++ bareflow-core/src/main/java/io/github/ss/bareflow/core/engine/StepEvaluator.java
@@ -1,5 +1,17 @@
 package io.github.ss.bareflow.core.engine;
 
-public class StepEvaluator {
+import java.util.Map;
 
+import io.github.ss.bareflow.core.context.ExecutionContext;
+
+public interface StepEvaluator {
+
+    /**
+     * Evaluate the input map of a step, resolving ${ctx.key} references.
+     *
+     * @param rawInput raw input map from StepDefinition
+     * @param ctx      execution context
+     * @return evaluated map ready for StepInvoker
+     */
+    Map<String, Object> evaluate(Map<String, Object> rawInput, ExecutionContext ctx);
 }
